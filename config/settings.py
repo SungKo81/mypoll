c9eb7df513d95eaf8c46f59c3649ab4617d0dc66
Ko Sungju <sungjuko@gmail.com>
2025-01-14 16:43:45 +0900

    2nd

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6ry+i+gqo128-m$jkp)w^_!2w#_ig^kgl(74ge&z8+)4(-xx@p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
